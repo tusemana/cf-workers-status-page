49f38f884cfbcec887515e572ade6c1d62de43a0
tusemana <140347698+tusemana@users.noreply.github.com>
2023-07-23 18:59:17 -0600

    Traducción de string al español

--- src/components/monitorStatusHeader.js
+++ src/components/monitorStatusHeader.js
@@ -23,9 +23,8 @@ export default function MonitorStatusHeader({ kvMonitorsLastUpdate }) {
         <div>{text}</div>
         {kvMonitorsLastUpdate.time && typeof window !== 'undefined' && (
           <div className="text-xs font-light">
-            checked{' '}
-            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} sec
-            ago (from{' '}
+            revisado{' '}
+            {Math.round((Date.now() - kvMonitorsLastUpdate.time) / 1000)} segundos atrás (from{' '}
             {locations[kvMonitorsLastUpdate.loc] || kvMonitorsLastUpdate.loc})
           </div>
         )}
